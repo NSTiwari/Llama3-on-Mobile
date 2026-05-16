3b971480f861841bb7caae1f17f4e29e07e3f138
NSTiwari <tiwarinitin1999@gmail.com>
2026-05-16 21:29:19 +0530

    Add quantize_and_convert.py, requirements.txt, and fix submodule

--- mobile-llama3/MobileLlama3/build/mlc_llm/tokenizers/sentencepiece/src/CMakeFiles/sentencepiece-static.dir/home/tiwarinitin1999/mlc-llm/3rdparty/tokenizers-cpp/sentencepiece/third_party/protobuf-lite/io_win32.cc.o.d
+++
@@ -1,2 +0,0 @@
-mlc_llm/tokenizers/sentencepiece/src/CMakeFiles/sentencepiece-static.dir/home/tiwarinitin1999/mlc-llm/3rdparty/tokenizers-cpp/sentencepiece/third_party/protobuf-lite/io_win32.cc.o: \
-  /home/tiwarinitin1999/mlc-llm/3rdparty/tokenizers-cpp/sentencepiece/third_party/protobuf-lite/io_win32.cc
